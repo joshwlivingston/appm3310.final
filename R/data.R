805e5c1272c59f98e8a8b704f53ec0c9efee00f5
Josh Livingston <joshwlivingston@gmail.com>
2024-04-27 22:16:40 -0600

    update ncaam data documentation

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
 #' NCAA Men's basketball data
 #'
-#' Collected and cleaned NCAA men's basketball data. This data is ready to be
-#' prepared for ranking using a `ranking_table_prep_*()` function. The data is at the
-#' team-game level. That is, there are two rows per game: one per team.
+#' Collected and cleaned NCAA men's 2023-24 regular season basketball data. This data is
+#' ready to be prepared for ranking using a `ranking_table_prep_*()` function. The data
+#' is at the team-game level. That is, there are two rows per game: one per team.
 #'
 #' @format ## `ncaam`
 #' A data frame with 10,654 rows and 7 columns:
